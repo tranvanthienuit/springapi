b5939d4a915f8a3ecc520a62d0dd58b1bf22fea0
Tran Van Thien <tranvanthienuit@gmail.com>
2022-06-07 15:28:38 +0700

    file

--- src/main/java/spring/Controller/HomeController.java
+++ src/main/java/spring/Controller/HomeController.java
@@ -54,7 +54,7 @@ public class HomeController {
     @Autowired
     RatingService ratingService;
 
-    @GetMapping(value = {"/trang-chu/{page}", "/trang-chu"})
+    @GetMapping(value = {"/{page}", "/"})
     public ResponseEntity<BookReturn> home(
             @PathVariable(name = "page", required = false) Integer page) throws Exception {
         BookReturn bookReturn = new BookReturn();
